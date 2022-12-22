2049f980abcd10c4963a6e8accc161cf28f345f8
mukesh <mukesh@mukeshs-MacBook-Pro.local>
2022-12-22 13:20:17 +0530

    largest common substring

--- common_substring.py
+++ common_substring.py
@@ -2,8 +2,9 @@ def find_common(strs):
     strs = sorted(strs)
     first = strs[0]
     last = strs[len(strs)-1]
-    
+
     n = min(len(first),len(last))
+    
     largest_substring = ""
     for i in range(n):
         if first[i] == last[i]:
@@ -14,7 +15,11 @@ def find_common(strs):
 
 
 
-my_input = [["aaa","aa","aaa"],["a","ab","abc"]]
-
+my_input = [["aaa","aa","aaa"],["a","ab","abc"],["dog","racecar","car"],["flower","flow","flight"]]
+case = 0 
 for i in my_input:
-    print(find_common(my_input))
+    case += 1
+    print(f"Test Case : {case}")
+    print(f"Input : {i}")
+    print(find_common(i))
+    print("------------------------------------------")
