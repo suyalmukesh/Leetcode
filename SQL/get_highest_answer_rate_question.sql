736143e62fc9b5e257d978dccbaf43e1472e9ae3
mukesh <suyalji85@gmail.com>
2023-01-06 19:41:17 +0530

    good day today

--- SQL/get_highest_answer_rate_question.sql
+++ SQL/get_highest_answer_rate_question.sql
@@ -32,3 +32,18 @@ solution as
     where final.log = (select max(final.log) from final)
 )
 select survey_log from solution where R = 1;
+
+
+=================
+
+Solution 2 : Copied 
+
+SELECT  question_id as survey_log
+FROM SurveyLog
+GROUP BY 1
+ORDER BY (count(answer_id) / count(case when action = 'show' then question_id ELSE null end )) desc, 1 asc
+LIMIT 1
+
+
+====================
+
