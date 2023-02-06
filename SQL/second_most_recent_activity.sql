ba5ce60a3c2830a2ac8c8438a06bce0920801f97
mukesh <suyalji85@gmail.com>
2023-02-06 09:13:22 +0530

    6Feb

---
+++ SQL/second_most_recent_activity.sql
@@ -0,0 +1,96 @@
+-- https://leetcode.com/problems/get-the-second-most-recent-activity/description/?envType=study-plan&id=sql-iii
+
+SQL Schema
+Table: UserActivity
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| username      | varchar |
+| activity      | varchar |
+| startDate     | Date    |
+| endDate       | Date    |
++---------------+---------+
+There is no primary key for this table. It may contain duplicates.
+This table contains information about the activity performed by each user in a period of time.
+A person with username performed an activity from startDate to endDate.
+ 
+
+Write an SQL query to show the second most recent activity of each user.
+
+If the user only has one activity, return that one. A user cannot perform more than one activity at the same time.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+UserActivity table:
++------------+--------------+-------------+-------------+
+| username   | activity     | startDate   | endDate     |
++------------+--------------+-------------+-------------+
+| Alice      | Travel       | 2020-02-12  | 2020-02-20  |
+| Alice      | Dancing      | 2020-02-21  | 2020-02-23  |
+| Alice      | Travel       | 2020-02-24  | 2020-02-28  |
+| Bob        | Travel       | 2020-02-11  | 2020-02-18  |
++------------+--------------+-------------+-------------+
+Output: 
++------------+--------------+-------------+-------------+
+| username   | activity     | startDate   | endDate     |
++------------+--------------+-------------+-------------+
+| Alice      | Dancing      | 2020-02-21  | 2020-02-23  |
+| Bob        | Travel       | 2020-02-11  | 2020-02-18  |
++------------+--------------+-------------+-------------+
+Explanation: 
+The most recent activity of Alice is Travel from 2020-02-24 to 2020-02-28, before that she was dancing from 2020-02-21 to 2020-02-23.
+Bob only has one record, we just take that one.
+
+-- DML 
+
+Create table If Not Exists UserActivity (username varchar(30), activity varchar(30), startDate date, endDate date)
+Truncate table UserActivity
+insert into UserActivity (username, activity, startDate, endDate) values ('Alice', 'Travel', '2020-02-12', '2020-02-20')
+insert into UserActivity (username, activity, startDate, endDate) values ('Alice', 'Dancing', '2020-02-21', '2020-02-23')
+insert into UserActivity (username, activity, startDate, endDate) values ('Alice', 'Travel', '2020-02-24', '2020-02-28')
+insert into UserActivity (username, activity, startDate, endDate) values ('Bob', 'Travel', '2020-02-11', '2020-02-18')
+
+
+-- My Solution 
+
+with cte as 
+(
+    select username , activity , startdate , enddate ,
+           row_number() over (partition by username order by enddate desc)R,
+           count(username) over (partition by username )RR
+    from useractivity
+)    
+select  username , activity , startdate , enddate
+from cte 
+where R = 2 
+union
+select  username , activity , startdate , enddate
+from cte 
+where R = 1 and RR = 1; 
+
+
+-- Without union
+
+with cte as 
+(
+    select username , activity , startdate , enddate ,
+           row_number() over (partition by username order by enddate desc)R,
+           count(username) over (partition by username )total_activity
+    from useractivity
+)    
+select  username , activity , startdate , enddate
+from cte 
+where (R = 2 and total_activity > 1) or (R = 1 and total_activity = 1);
+
+         
+
+         
+
